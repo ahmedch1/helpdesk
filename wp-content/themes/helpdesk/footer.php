e3d4c030859601d85d3873bd0dc09b5db60ba1c6
Ahmed Ch <ahmedchouihi@gmail.com>
2022-08-07 14:54:02 +0100

    removeFooterAddSiteTitleFooter

--- wp-content/themes/helpdesk/footer.php
+++ wp-content/themes/helpdesk/footer.php
@@ -13,17 +13,7 @@
 
 	<footer id="colophon" class="site-footer">
 		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'helpdesk' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'helpdesk' ), 'WordPress' );
-				?>
-			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'helpdesk' ), 'helpdesk', '<a href="http://underscores.me/">Underscores.me</a>' );
-				?>
+				<p><?php echo get_bloginfo( 'name') . ' - '; ?>Copyright  <?php echo ' ' . date('Y') ;?></p>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
